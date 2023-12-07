6be77ce451a1bab5568fe270bf2732c78b5c1cea
Yaniss G.M. Chioukh <chioukh.yanis@hotmail.fr>
2023-12-07 11:10:21 +0000

    add operators, equal, delete and clear functions

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const multiply = (a, b)  => (a * b);
 const divide = (a, b) => (a / b);
 const calculatePercentage = a => (a / 100);
 
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
 
-function operate(num1, mathOperator, num2) {
+function operate(num1, selectedOperator, num2) {
     let result;
-    switch (mathOperator) {
+    switch (selectedOperator) {
         case '+':
             result = add(num1, num2);
             break;
@@ -40,15 +40,65 @@ numberButtons.forEach(button => {
 });
 
 function displayClickedNumber(clickedNumber) {
-    if (displayValue === 0) {
+    if (displayValue.length >= 12) {
+        displayValue = 'DIGITS LIMIT';
+    } else if (displayValue === 0) {
         displayValue = clickedNumber;
     } else {
         displayValue += clickedNumber
     };
-
     updateDisplay();
-}
+};
 
 function updateDisplay() {
     document.querySelector('.display').textContent = displayValue;
-}
+};
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (firstNumber === null) {
+            firstNumber = parseFloat(displayValue);
+            operator = button.textContent;
+            displayValue = '';
+            updateDisplay();
+        } else {
+            secondNumber = parseFloat(displayValue);
+            firstNumber = operate(firstNumber, operator, secondNumber);
+            operator = button.textContent;
+            secondNumber = null;
+            displayValue = '';
+            updateDisplay();
+        }
+    });
+});
+
+const equalButton = document.querySelector('#equal');
+equalButton.addEventListener('click', () => {
+    if (firstNumber !== null && operator !== null) {
+        secondNumber = parseFloat(displayValue);
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        operator = null;
+        secondNumber = null;
+        displayValue = firstNumber.toString()
+        updateDisplay();
+    }
+});
+
+const deleteButton = document.querySelector('#delete');
+deleteButton.addEventListener('click', () => {
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
+    displayValue = 0;
+    updateDisplay();
+});
+
+
+updateDisplay();
